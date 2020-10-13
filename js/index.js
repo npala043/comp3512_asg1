11fc78175b680f00e078b42be37c0392425367bb
npala043 <npala043@mtroyal.ca>
2020-10-12 18:13:00 -0600

    same as last, not sure why it didn't go through

--- js/index.js
+++ js/index.js
@@ -10,19 +10,24 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector("#single").style.display = "grid";
     });
 
-    // fetch info from API
+    // Fetch info from API
     const url = "https://www.randyconnolly.com/funwebdev/3rd/api/art/galleries.php";
     const response = fetch(url)
         .then(response => response.json())
         .then(data => popGalleries(data))
         .catch(err => console.error(err));
+    // For the above (and below), Nahuel used:
+    // https://css-tricks.com/using-fetch/
+    // https://www.youtube.com/watch?v=QO4NXhWo_NM
+    // https://stackoverflow.com/questions/38380462/syntaxerror-unexpected-token-o-in-json-at-position-1
+    // to understand what to do
+
 
     // Display List of Galleries
     function popGalleries(data) {
-        // const galleries = JSON.parse(data);
         let list = ``;
         for (let g of data) {
-            list += `<p>${g.GalleryName}</p><br/>`;
+            list += `<p>${g.GalleryName}</p><br>`;
         }
         document.querySelector("#galleries").innerHTML = list;
     }
