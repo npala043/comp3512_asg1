bbcbfcc875b480d5665d8b47b9d2941de1f5fd84
npala043 <npala043@mtroyal.ca>
2020-10-15 13:23:08 -0600

    refactored and starting single painting view

--- js/index.js
+++ js/index.js
@@ -2,9 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const galleriesURL = "https://www.randyconnolly.com/funwebdev/3rd/api/art/galleries.php";
     const infoURL = 'https://www.randyconnolly.com/funwebdev/3rd/api/art/paintings.php?gallery='; // append galleryID here
-    //const imagesURL = 'https://res.cloudinary.com/funwebdev/image/upload/SIZE/art/paintings/FILENAME';
+    // Image url https://res.cloudinary.com/funwebdev/image/upload/SIZE/art/paintings/FILENAME
 
-    // Fetch Galleries
     const galleries = document.querySelector("#galleries");
     const info = document.querySelector("#info");
     const mapDiv = document.querySelector("#map");
@@ -16,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const load4 = document.querySelector("#loader4");
     const pTable = document.querySelector("#paintingTable");
 
+    // Fetch Galleries
     fetch(galleriesURL)
         .then(response => response.json())
         .then(data => {
@@ -29,9 +29,9 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelectorAll(".galleryNames").forEach(g => {
                 g.addEventListener("click", function (e) {
                     // display all loading gifs
-                    load2.style.display = "";
-                    load3.style.display = "";
-                    load4.style.display = "";
+                    load2.style.display = "block";
+                    load3.style.display = "block";
+                    load4.style.display = "block";
 
                     // Gallery Info
                     displayGalleryInfo(data, e.target.id);
@@ -46,22 +46,21 @@ document.addEventListener("DOMContentLoaded", function () {
                     load4.style.display = "none";
                     pTable.style.display = "";
                 });
-            })
+            });
         })
         .catch(err => console.error(err));
 
-    function displayMap(data, id) {
+    // ------------------------- Helper Functions ------------------------- //
 
-    }
+    // function displayMap(data, id) {
 
-
+    // }
 
     function displayPaintings(id) {
         fetch(infoURL + id)
             .then(response => response.json())
-            .then(data => {
-                displayPaintingTable(data);
-            })
+            .then(data => displayPaintingTable(data))
+            .catch(err => console.error(err));
     }
 
     function displayPaintingTable(paintings) {
@@ -78,18 +77,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Artist (last) name
             const tdName = document.createElement("td");
-            const name = document.createTextNode(`${p.LastName}`);
-            tdName.appendChild(name);
+            tdName.appendChild(document.createTextNode(`${p.LastName}`));
 
             // Painting title
             const tdTitle = document.createElement("td");
+            tdTitle.setAttribute("style", "cursor:pointer; color:blue; text-decoration:underline;");
+            tdTitle.classList.add("paintingTitle");
             const title = document.createTextNode(`${p.Title}`);
             tdTitle.appendChild(title);
 
             // Painting year
             const tdYear = document.createElement("td");
-            const year = document.createTextNode(`${p.YearOfWork}`);
-            tdYear.appendChild(year);
+            tdYear.appendChild(document.createTextNode(`${p.YearOfWork}`));
 
             tr.appendChild(tdImg);
             tr.appendChild(tdName);
@@ -97,34 +96,47 @@ document.addEventListener("DOMContentLoaded", function () {
             tr.appendChild(tdYear);
             pTable.appendChild(tr);
         });
+        applyPaintingEventHandlers(paintings);
     }
 
     function addTableHeaders() {
         const tr = document.createElement("tr");
-        
-        // Image header
-        const thImg = document.createElement("th");
 
         // Artist Header
         const thArtist = document.createElement("th");
-        const artist = document.createTextNode("Artist")
-        thArtist.appendChild(artist);
+        thArtist.appendChild(document.createTextNode("Artist"));
 
         // Title Header
         const thTitle = document.createElement("th");
-        const title = document.createTextNode("Title");
-        thTitle.appendChild(title);
+        thTitle.appendChild(document.createTextNode("Title"));
 
         // Year Header
         const thYear = document.createElement("th");
-        const year = document.createTextNode("Year");
-        thYear.appendChild(year);
+        thYear.appendChild(document.createTextNode("Year"));
 
-        tr.appendChild(thImg);
+        tr.appendChild(document.createElement("th"));
         tr.appendChild(thArtist);
         tr.appendChild(thTitle);
         tr.appendChild(thYear);
         paintingTable.appendChild(tr);
+    }
+
+    function applyPaintingEventHandlers(data) {
+        const titles = document.querySelectorAll(".paintingTitle");
+        titles.forEach(t => {
+            t.addEventListener("click", function (e) {
+                const divs = document.querySelectorAll(".listView");
+                divs.forEach(d => d.style.display = "none");
+                document.querySelector("#single").style.display = "grid";
+                displaySinglePainting(data.find(d => d.Title == e.target.innerHTML).ImageFileName);
+            });
+        });
+    }
+
+    function displaySinglePainting(fileName) {
+        const img = document.createElement("img");
+        img.src = `https://res.cloudinary.com/funwebdev/image/upload/w_500/art/paintings/${fileName}`;
+        single.appendChild(img);
     }
 
     function displayGalleryList(data) {
@@ -146,27 +158,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
         //Creates Title
         const h2 = document.createElement("h2");
-        const h2Title = document.createTextNode("Gallery Information:");
-        h2.appendChild(h2Title);
+        h2.appendChild(document.createTextNode("Gallery Information:"));
         info.appendChild(h2);
 
-        //Creates Gallery Name (Native Name)
+        //Creates Gallery Name *Only display native name if diff than English name*
         const pName = document.createElement("p");
-        const name = document.createTextNode(`${gallery.GalleryName} (${gallery.GalleryNativeName})`);
+        const name = (gallery.GalleryName == gallery.GalleryNativeName) ?
+            document.createTextNode(`${gallery.GalleryName}`) :
+            document.createTextNode(`${gallery.GalleryName} (${gallery.GalleryNativeName})`);
         pName.appendChild(name);
         info.appendChild(pName);
 
-
         //Creates Gallery Address
         const pAddress = document.createElement("p");
-        const address = document.createTextNode(`${gallery.GalleryAddress}`);
-        pAddress.appendChild(address);
+        pAddress.appendChild(document.createTextNode(`${gallery.GalleryAddress}`));
         info.appendChild(pAddress);
 
         //Creates Gallery Country
         const pCountry = document.createElement("p");
-        const country = document.createTextNode(`${gallery.GalleryCity}, ${gallery.GalleryCountry}`);
-        pCountry.appendChild(country);
+        pCountry.appendChild(document.createTextNode(`${gallery.GalleryCity}, ${gallery.GalleryCountry}`));
         info.appendChild(pCountry);
 
         //Creates Gallery Website
@@ -176,21 +186,12 @@ document.addEventListener("DOMContentLoaded", function () {
         info.appendChild(pWebsite);
     }
 
-    // Change to Single Painting view
-    // const button = document.querySelector("#singleView");
-    // button.addEventListener("click", function () {
-    //     const divs = document.querySelectorAll(".listView");
-    //     divs.forEach(d => d.style.display = "none");
-    //     document.querySelector("#single").style.display = "grid";
-    // });
-
-   
 });
 
-let map;
-function initMap() {
-    map = new google.maps.Map(document.querySelector('#gMap'), {
-        center: { lat: 41.89474, lng: 12.4839 },
-        zoom: 6
-    });
-}
+// let map;
+// function initMap() {
+//     map = new google.maps.Map(document.querySelector('#gMap'), {
+//         center: { lat: 41.89474, lng: 12.4839 },
+//         zoom: 6
+//     });
+// }
